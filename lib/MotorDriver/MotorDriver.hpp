6195176129bf1156488f86597a9d68668f4b4f71
Luis Venancio <venancio@lsts.pt>
2023-04-06 11:30:20 +0100

    MotorDriver: Style cleanup.

--- lib/MotorDriver/MotorDriver.hpp
+++ lib/MotorDriver/MotorDriver.hpp
@@ -8,15 +8,33 @@ class MotorDriver
 {
   public:
     MotorDriver(Adafruit_PWMServoDriver* driver, uint8_t enable_pin, uint8_t phase_pin, uint8_t sleep_pin);
-    void begin();
-    void brake();
-    void turnOn();
-    void turnOff();
-    void setSpeed(uint16_t speed);
-    void setDirection(bool direction);
-    void switchDirection();
-    uint8_t getSpeed();
-    bool getDirection();
+
+    void 
+    begin();
+
+    void 
+    brake();
+
+    void 
+    turnOn();
+
+    void 
+    turnOff();
+
+    void 
+    setSpeed(uint16_t speed);
+
+    void 
+    setDirection(bool direction);
+
+    void 
+    switchDirection();
+
+    uint8_t 
+    getSpeed();
+
+    bool 
+    getDirection();
 
   private:
     Adafruit_PWMServoDriver* m_driver;
